f4503ca68839e872930d87857904dcd8fdc7ae8d
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-10-01 17:59:20 -0400

    Init: archetype generation

---
+++ src/main/java/com/comp550proj/App.java
@@ -0,0 +1,13 @@
+package com.comp550proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
