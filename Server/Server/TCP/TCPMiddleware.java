1787734f212970455064a99ce95f4e63f2b25234
dliuproduction <dliuproduction@gmail.com>
2018-10-03 02:06:04 -0400

    add template TCP files

---
+++ Server/Server/TCP/TCPMiddleware.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+ 
+/**
+ * This program demonstrates a simple TCP/IP socket server that echoes every
+ * message from the client in reversed form.
+ * This server is multi-threaded.
+ *
+ * @author www.codejava.net
+ */
+public class ReverseServer {
+ 
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+ 
+        int port = Integer.parseInt(args[0]);
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+ 
+                new ServerThread(socket).start();
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
