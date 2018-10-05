06e0a39f10cf0e7678ebd637201482d237887ac0
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-10-05 01:20:03 -0400

    Fix: maven path for java execution

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,4 +1,4 @@
 #Usage: ./run_server.sh [<rmi_name>]
 
 ./run_rmi.sh > /dev/null 2>&1
-java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.RMIResourceManager $1
+java -cp target/classes -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$(pwd)../src/main/java/Server/ Server.RMI.RMIResourceManager $1
