41977c9d19647725f41f3a25296f8f61d7d496b4
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-11-05 22:01:04 -0500

    New: HashTable for involved ResourceManagers

--- src/main/java/Server/LockManager/TransactionManager.java
+++ src/main/java/Server/LockManager/TransactionManager.java
@@ -1,5 +1,7 @@
 package Server.LockManager;
 
+import java.util.*;
+
 import Server.Common.*;
 
 public class TransactionManager {
@@ -7,16 +9,18 @@ public class TransactionManager {
   private static int TABLE_SIZE = 2039;
 
   private static TPHashTable activeTransactions = new TPHashTable(TransactionManager.TABLE_SIZE);
+  private static RMHashTable involvedResourceManagers = new HashTable<Integer, ResourceManager[]>();
 
   private static IncrementingInteger txidPicker = new IncrementingInteger();
 
   public void TransactionManager() {
   }
 
-  public boolean start(ResourceManager rm) throws RemoteException {
+  public boolean start(ResourceManager[] resourceManagers) throws RemoteException {
       int txid = txidPicker.pick();
       TransactionObject tx = new TransactionObject(txid);
 
-
+      activeTransactions.add(tx);
+      involvedResourceManagers.put(tx.getXId(), resourceManagers);
   }
 }
